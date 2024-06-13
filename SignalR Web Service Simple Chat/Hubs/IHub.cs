841685d82dd60a6273aae5ece02d1a9f091ec14a
RysteQ <34135554+RysteQ@users.noreply.github.com>
2024-06-13 21:22:46 +0300

    Started to slowly work on the hub for the SignalR

---
+++ SignalR Web Service Simple Chat/Hubs/IHub.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR_Web_Service_Simple_Chat.Hubs;
+
+public interface IHub
+{
+    public void SendMessage(int userId, string message);
+    public void BroadcastMessage(string message);
+    public void GetUsers();
+    public void MuteUser(int userId);
+    public void UnmuteUser(int userId);
+    public void KickUser(int userId);
+}
---
+++ SignalR Web Service Simple Chat/Hubs/MainMessageHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR_Web_Service_Simple_Chat.Hubs;
+using SignalR_Web_Service_Simple_Chat.Models;
+
+namespace SignalR_Web_Service_Simple_Chat.Hubsl;
+
+public class MainMessageHub : Hub<IHub>
+{
+    public override Task OnConnectedAsync()
+    {
+        _users.Add(new()
+        {
+            UserId = _currentUserId,
+            ConnectionId = Context.ConnectionId,
+        });
+
+        _currentUserId++;
+
+        return base .OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        _users.Remove(_users.Where(selectedUser => selectedUser.ConnectionId == Context.ConnectionId).First());
+
+        return base.OnDisconnectedAsync(exception);
+    }
+
+    private List<UserViewModel> _users = [];
+    private int _currentUserId;
+}
