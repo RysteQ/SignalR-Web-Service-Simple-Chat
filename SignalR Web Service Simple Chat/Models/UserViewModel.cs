8f82e2642c0b3aff02d227afda27e5483cc6faa2
RysteQ <34135554+RysteQ@users.noreply.github.com>
2024-06-13 21:22:59 +0300

    Made the user view model to store the user data

---
+++ SignalR Web Service Simple Chat/Models/UserViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SignalR_Web_Service_Simple_Chat.Models;
+
+public record UserViewModel
+{
+    public required int UserId { get; set; }
+    public required string ConnectionId { get; set; }
+}
