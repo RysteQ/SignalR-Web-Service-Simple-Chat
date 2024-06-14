6f28c1ff66b9154c8affc810db07b43ae1be7dad
RysteQ <34135554+RysteQ@users.noreply.github.com>
2024-06-14 19:46:04 +0300

    Created the SignalR client

--- SignalR Client/Program.cs
+++ SignalR Client/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+Console.Write("IP -> ");
+string? ip = Console.ReadLine();
+
+Console.Write("Port -> ");
+string? port = Console.ReadLine();
+
+if (string.IsNullOrEmpty(ip) || string.IsNullOrEmpty(port))
+{
+    Console.WriteLine("Please provide a valid IP and port");
+    Console.ReadKey();
+
+    Environment.Exit(-1);
+}
+
+try
+{
+    HubConnection hubConnection = new HubConnectionBuilder().WithUrl($"http://{ip}:{port}/chat").Build();
+    await hubConnection.StartAsync();
+
+    hubConnection.On<string>("ReceiveMessage", (message) =>
+    {
+        Console.WriteLine(message);
+    });
+
+    hubConnection.On<string>("Kick", (reason) =>
+    {
+        Console.WriteLine(reason);
+        Console.ReadKey();
+
+        Environment.Exit(0);
+    });
+
+    while (true)
+    {
+        await Task.Delay(100);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+    Console.ReadKey();
+
+    Environment.Exit(-1);
+}
